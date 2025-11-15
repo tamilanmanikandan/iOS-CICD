46c66cc5132de95d4094bd61f555145eaf174421
Manikandan P <manikandanp@Manikandans-Mac-mini.local>
2025-11-15 09:55:19 +0530

    Initial commit

---
+++ iOS-CICD/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iOS-CICD
+//
+//  Created by Manikandan P on 15/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iOS-CICD/iOS_CICDApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_CICDApp.swift
+//  iOS-CICD
+//
+//  Created by Manikandan P on 15/11/25.
+//
+
+import SwiftUI
+
+@main
+struct iOS_CICDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
